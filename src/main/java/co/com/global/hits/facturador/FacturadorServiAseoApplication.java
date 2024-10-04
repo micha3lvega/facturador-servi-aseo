1c84b0fc1f63c8e43a42781ee6f83bdb5de38082
Michael Vega <micha3lvega@gmail.com>
2024-10-04 16:32:48 -0500

    Creación de proyecto

---
+++ src/main/java/co/com/global/hits/facturador/FacturadorServiAseoApplication.java
@@ -0,0 +1,13 @@
+package co.com.global.hits.facturador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturadorServiAseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturadorServiAseoApplication.class, args);
+	}
+
+}
